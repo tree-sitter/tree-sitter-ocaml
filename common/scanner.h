57644edfbba0edb38ac17dba2add4c243fa3539b
Pieter Goetschalckx <github@gtschlckx.be>
2025-01-08 22:06:37 +0100

    External scanner improvements (#107)
    * Support unicode identifiers in comments
    
    * Fix quoted strings with unicode id
    
    * Use sentinel for error recovery
    
    * Don't allow multi-byte character literals
    
    * Handle newline normalization in character literals
    
    * Allow unclosed comments
    
    * Don't serialize unused quoted_string_id
    
    * Generate grammars

--- common/scanner.h
+++ common/scanner.h
@@ -14,7 +14,8 @@ enum TokenType {
   RIGHT_QUOTED_STRING_DELIM,
   STRING_DELIM,
   LINE_NUMBER_DIRECTIVE,
-  NULL_CHARACTER
+  NULL_CHARACTER,
+  ERROR_SENTINEL,
 };
 
 typedef struct {
@@ -89,37 +90,178 @@ static void scan_string(TSLexer *lexer) {
   }
 }
 
-static bool is_quoted_string_delim(int32_t c) {
-    return iswlower(c) || c == '_' || c >= 128;
+static const int32_t LOWER_UTF8_CHARS[] = {
+    // https://github.com/ocaml/ocaml/blob/5.3.0-rc1/utils/misc.ml#L281-L298
+    0xdf,   // ß
+    0xe0,   // à
+    0xe1,   // á
+    0xe2,   // â
+    0xe3,   // ã
+    0xe4,   // ä
+    0xe5,   // å
+    0xe6,   // æ
+    0xe7,   // ç
+    0xe8,   // è
+    0xe9,   // é
+    0xea,   // ê
+    0xeb,   // ë
+    0xec,   // ì
+    0xed,   // í
+    0xee,   // î
+    0xef,   // ï
+    0xf0,   // ð
+    0xf1,   // ñ
+    0xf2,   // ò
+    0xf3,   // ó
+    0xf4,   // ô
+    0xf5,   // õ
+    0xf6,   // ö
+    0xf8,   // ø
+    0xf9,   // ù
+    0xfa,   // ú
+    0xfb,   // û
+    0xfc,   // ü
+    0xfd,   // ý
+    0xfe,   // þ
+    0xff,   // ÿ
+    0x153,  // œ
+    0x161,  // š
+    0x17e,  // ž
+};
+
+static const int32_t LOWER_UTF8_PAIRS[][3] = {
+    // https://github.com/ocaml/ocaml/blob/5.3.0-rc1/utils/misc.ml#L326-L340
+    {'a', 0x300, 0xe0},   // à
+    {'a', 0x301, 0xe1},   // á
+    {'a', 0x302, 0xe2},   // â
+    {'a', 0x303, 0xe3},   // ã
+    {'a', 0x308, 0xe4},   // ä
+    {'a', 0x30a, 0xe5},   // å
+    {'c', 0x327, 0xe7},   // ç
+    {'e', 0x300, 0xe8},   // è
+    {'e', 0x301, 0xe9},   // é
+    {'e', 0x302, 0xea},   // ê
+    {'e', 0x308, 0xeb},   // ë
+    {'i', 0x300, 0xec},   // ì
+    {'i', 0x301, 0xed},   // í
+    {'i', 0x302, 0xee},   // î
+    {'i', 0x308, 0xef},   // ï
+    {'n', 0x303, 0xf1},   // ñ
+    {'o', 0x300, 0xf2},   // ò
+    {'o', 0x301, 0xf3},   // ó
+    {'o', 0x302, 0xf4},   // ô
+    {'o', 0x303, 0xf5},   // õ
+    {'o', 0x308, 0xf6},   // ö
+    {'s', 0x30c, 0x161},  // š
+    {'u', 0x300, 0xf9},   // ù
+    {'u', 0x301, 0xfa},   // ú
+    {'u', 0x302, 0xfb},   // û
+    {'u', 0x308, 0xfc},   // ü
+    {'y', 0x301, 0xfd},   // ý
+    {'y', 0x308, 0xff},   // ÿ
+    {'z', 0x30c, 0x17e},  // ž
+};
+
+static inline int32_t search_lower_utf8_char(const int32_t c) {
+  const int32_t *base = LOWER_UTF8_CHARS;
+  const size_t size = sizeof(LOWER_UTF8_CHARS) / sizeof(LOWER_UTF8_CHARS[0]);
+
+  for (size_t range = size; range != 0; range >>= 1) {
+    const int32_t *probe = base + (range >> 1);
+    if (c == *probe) {
+      return *probe;
+    } else if (c > *probe) {
+      base = probe + 1;
+      range--;
+    }
+  }
+
+  return 0;
+}
+
+static inline int32_t search_lower_utf8_pair(const int32_t c,
+                                             const int32_t diacritic) {
+  const int32_t(*base)[3] = LOWER_UTF8_PAIRS;
+  const size_t size = sizeof(LOWER_UTF8_PAIRS) / sizeof(LOWER_UTF8_PAIRS[0]);
+
+  for (size_t range = size; range != 0; range >>= 1) {
+    const int32_t *probe = base[range >> 1];
+
+    if (c == probe[0] && diacritic == probe[1]) {
+      return probe[2];
+    } else if (c > probe[0] || (c == probe[0] && diacritic > probe[1])) {
+      base = base + (range >> 1) + 1;
+      range--;
+    }
+  }
+
+  return 0;
+}
+
+static inline int32_t scan_quoted_string_delim_char(TSLexer *lexer) {
+  const int32_t c = lexer->lookahead;
+  int32_t r;
+
+  if (c == '|') return 0;
+
+  if (c == '_') {
+    advance(lexer);
+    return c;
+  }
+
+  if (c >= 'a' && c <= 'z') {
+    advance(lexer);
+
+    if (lexer->lookahead >= 0x300 && lexer->lookahead <= 0x327 &&
+        (r = search_lower_utf8_pair(c, lexer->lookahead))) {
+      advance(lexer);
+      return r;
+    } else {
+      return c;
+    }
+  }
+
+  if ((r = search_lower_utf8_char(c))) {
+    advance(lexer);
+    return r;
+  }
+
+  return 0;
 }
 
 static bool scan_left_quoted_string_delimiter(Scanner *scanner,
                                               TSLexer *lexer) {
-  quoted_string_id_clear(scanner);
+  int32_t c;
 
-  while (is_quoted_string_delim(lexer->lookahead)) {
-    quoted_string_id_push(scanner, lexer->lookahead);
-    advance(lexer);
+  while ((c = scan_quoted_string_delim_char(lexer))) {
+    quoted_string_id_push(scanner, c);
   }
 
-  if (lexer->lookahead != '|') return false;
-
-  advance(lexer);
-  scanner->in_string = true;
-  return true;
+  if (lexer->lookahead == '|') {
+    advance(lexer);
+    scanner->in_string = true;
+    return true;
+  } else {
+    quoted_string_id_clear(scanner);
+    return false;
+  }
 }
 
 static bool scan_right_quoted_string_delimiter(Scanner *scanner,
                                                TSLexer *lexer) {
   for (size_t i = 0; i < scanner->quoted_string_id_length; i++) {
-    if (lexer->lookahead != scanner->quoted_string_id[i]) return false;
-    advance(lexer);
+    if (scan_quoted_string_delim_char(lexer) != scanner->quoted_string_id[i]) {
+      return false;
+    }
   }
 
-  if (lexer->lookahead != '}') return false;
-
-  scanner->in_string = false;
-  return true;
+  if (lexer->lookahead == '}') {
+    scanner->in_string = false;
+    quoted_string_id_clear(scanner);
+    return true;
+  } else {
+    return false;
+  }
 }
 
 static bool scan_quoted_string(Scanner *scanner, TSLexer *lexer) {
@@ -193,13 +335,23 @@ static int32_t scan_character(TSLexer *lexer) {
       break;
     case '\'':
       break;
+    case '\r':
+      advance(lexer);
+      while (lexer->lookahead == '\r') advance(lexer);
+      if (lexer->lookahead != '\n') return 0;
+      advance(lexer);
+      break;
     case '\0':
       if (eof(lexer)) return 0;
       advance(lexer);
       break;
     default:
-      last = lexer->lookahead;
-      advance(lexer);
+      if (lexer->lookahead < 256) {
+        last = lexer->lookahead;
+        advance(lexer);
+      } else {
+        return 0;
+      }
   }
 
   if (lexer->lookahead == '\'') {
@@ -209,19 +361,28 @@ static int32_t scan_character(TSLexer *lexer) {
   return last;
 }
 
-static bool scan_identifier(TSLexer *lexer) {
-  if (iswalpha(lexer->lookahead) || lexer->lookahead == '_') {
+static inline bool is_lowercase_ext(const int32_t c) {
+  return (c >= 'a' && c <= 'z') || c == '_' || c >= 192;
+}
+
+static inline bool is_identstart(const int32_t c) {
+  return is_lowercase_ext(c) || (c >= 'A' && c <= 'Z');
+}
+
+static inline bool is_identchar(const int32_t c) {
+  return is_identstart(c) || (c >= '0' && c <= '9') || c == '\'';
+}
+
+static inline bool scan_identifier(TSLexer *lexer) {
+  if (is_identstart(lexer->lookahead)) {
     advance(lexer);
-    while (iswalnum(lexer->lookahead) || lexer->lookahead == '_' ||
-           lexer->lookahead == '\'') {
-      advance(lexer);
-    }
+    while (is_identchar(lexer->lookahead)) advance(lexer);
     return true;
   }
   return false;
 }
 
-static bool scan_extattrident(TSLexer *lexer) {
+static inline bool scan_extattrident(TSLexer *lexer) {
   while (scan_identifier(lexer)) {
     if (lexer->lookahead != '.') return true;
     advance(lexer);
@@ -290,7 +451,7 @@ static bool scan_comment(Scanner *scanner, TSLexer *lexer) {
         if (scan_quoted_string(scanner, lexer)) advance(lexer);
         break;
       case '\0':
-        if (eof(lexer)) return false;
+        if (eof(lexer)) return true;
         if (last) {
           last = 0;
         } else {
@@ -319,11 +480,12 @@ static void destroy(Scanner *scanner) {
 
 static unsigned serialize(Scanner *scanner, char *buffer) {
   buffer[0] = scanner->in_string;
-  if (scanner->quoted_string_id_length >=
+  if (scanner->quoted_string_id_length <
       TREE_SITTER_SERIALIZATION_BUFFER_SIZE) {
+    return quoted_string_id_copy(scanner, buffer + 1) + 1;
+  } else {
     return 1;
   }
-  return quoted_string_id_copy(scanner, buffer + 1) + 1;
 }
 
 static void deserialize(Scanner *scanner, const char *buffer, unsigned length) {
@@ -337,16 +499,20 @@ static void deserialize(Scanner *scanner, const char *buffer, unsigned length) {
 }
 
 static bool scan(Scanner *scanner, TSLexer *lexer, const bool *valid_symbols) {
-  if (valid_symbols[LEFT_QUOTED_STRING_DELIM] &&
-      (is_quoted_string_delim(lexer->lookahead) || lexer->lookahead == '|')) {
+  if (!valid_symbols[ERROR_SENTINEL] &&
+      valid_symbols[LEFT_QUOTED_STRING_DELIM] &&
+      (is_lowercase_ext(lexer->lookahead) || lexer->lookahead == '|')) {
     lexer->result_symbol = LEFT_QUOTED_STRING_DELIM;
     return scan_left_quoted_string_delimiter(scanner, lexer);
   }
-  if (valid_symbols[RIGHT_QUOTED_STRING_DELIM] && (lexer->lookahead == '|')) {
+
+  if (!valid_symbols[ERROR_SENTINEL] &&
+      valid_symbols[RIGHT_QUOTED_STRING_DELIM] && (lexer->lookahead == '|')) {
     advance(lexer);
     lexer->result_symbol = RIGHT_QUOTED_STRING_DELIM;
     return scan_right_quoted_string_delimiter(scanner, lexer);
   }
+
   if (scanner->in_string && valid_symbols[STRING_DELIM] &&
       lexer->lookahead == '"') {
     advance(lexer);
@@ -359,8 +525,16 @@ static bool scan(Scanner *scanner, TSLexer *lexer, const bool *valid_symbols) {
     skip(lexer);
   }
 
-  if (!scanner->in_string && lexer->lookahead == '#' &&
-      lexer->get_column(lexer) == 0) {
+  if (!scanner->in_string && valid_symbols[STRING_DELIM] &&
+      lexer->lookahead == '"') {
+    advance(lexer);
+    scanner->in_string = true;
+    lexer->result_symbol = STRING_DELIM;
+    return true;
+  }
+
+  if (!scanner->in_string && valid_symbols[LINE_NUMBER_DIRECTIVE] &&
+      lexer->lookahead == '#' && lexer->get_column(lexer) == 0) {
     advance(lexer);
 
     while (lexer->lookahead == ' ' || lexer->lookahead == '\t') {
@@ -393,18 +567,14 @@ static bool scan(Scanner *scanner, TSLexer *lexer, const bool *valid_symbols) {
     lexer->result_symbol = LINE_NUMBER_DIRECTIVE;
     return true;
   }
-  if (!scanner->in_string && lexer->lookahead == '(') {
+
+  if (!scanner->in_string && valid_symbols[COMMENT] &&
+      lexer->lookahead == '(') {
     advance(lexer);
     lexer->result_symbol = COMMENT;
     return scan_comment(scanner, lexer);
   }
-  if (!scanner->in_string && valid_symbols[STRING_DELIM] &&
-      lexer->lookahead == '"') {
-    advance(lexer);
-    scanner->in_string = true;
-    lexer->result_symbol = STRING_DELIM;
-    return true;
-  }
+
   if (valid_symbols[NULL_CHARACTER] && lexer->lookahead == '\0' &&
       !eof(lexer)) {
     advance(lexer);
